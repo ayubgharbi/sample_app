c383642b5dc06a307dbe6c3b2ddd8f901577be99
Gharbi Ayoub <ayoub97ma@gmail.com>
2016-01-30 20:20:57 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  def hello 
-  	render text: "hello, world!"
-  end
   protect_from_forgery with: :exception
 end
