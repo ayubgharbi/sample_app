a4f984c1ac60d8ee61c8400ffbac02f36cdd73cb
Gharbi Ayoub <ayoub97ma@gmail.com>
2016-04-27 13:31:57 +0100

    Finish chapter 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -56,13 +56,13 @@ module SessionsHelper
 	end
 
 	# Redirects to stored location (or to the default).
-	def redirect_back_or(default)
-		redirect_to(session[:forwarding_url] || default)
-		session.delete(:forwarding_url)
-	end
+  	def redirect_back_or(default)
+    	redirect_to(session[:forwarding_url] || default)
+    	session.delete(:forwarding_url)
+  	end
 
-	# Stores the URL trying to be accessed.
-	def store_location
-		session[:forwarding_url]= request.url if request.get?
-	end
+  # Stores the URL trying to be accessed.
+  	def store_location
+    	session[:forwarding_url] = request.url if request.get?
+  	end
 end
